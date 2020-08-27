0dcf3b1f3ea097aa2142b1b5c73002b071b65fe9
sergystepanov <sergystepanov@gmail.com>
2020-08-27 23:34:47 +0300

    Add media module exports

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import 'webrtc-adapter';
 export { default as WebRTC } from './network/webrtc';
 export { default as SignallingBuilder } from './network/protocol/signalling';
 export { default as Socket } from './network/transport/socket/socket';
+export { default as Stream } from './media/stream/stream';
+export { default as UserMedia } from './media/user/userMedia';
